dacbb00271f247e0fe6cd1f5c8711b93d5fba630
davidnmora <davidnmora@gmail.com>
2020-04-07 09:09:26 -0700

    Update default fields to match County Preparedness popup

--- src/constants.js
+++ src/constants.js
@@ -12,15 +12,16 @@ export const DEFAULT_INPUT_VALUES = [
 		fieldDisplayName: 'prep_score',
 	},
 	
+	// "Pillar" scores
 	{
 		fieldType: FIELD_TYPES.scoreOutOf100,
 		fieldName: 'bed_score_1',
-		fieldDisplayName: 'ICU Bed score',
+		fieldDisplayName: 'Physical Capacity score',
 	},
 	{
 		fieldType: FIELD_TYPES.scoreOutOf100,
 		fieldName: 'staff_score_1',
-		fieldDisplayName: 'Critical Staff score',
+		fieldDisplayName: 'Human Resources score',
 	},
 	{
 		fieldType: FIELD_TYPES.scoreOutOf100,
@@ -38,11 +39,31 @@ export const DEFAULT_INPUT_VALUES = [
 		fieldDisplayName: 'COVID-19 score',
 	},
 	
-	
+	// Numeric
 	{
 		fieldType: FIELD_TYPES.number,
-		fieldName: 'total_staff_100k',
-		fieldDisplayName: 'Staff per 100k',
+		fieldName: 'total_staff_dt_100k',
+		fieldDisplayName: 'Staff w/in 40min per 100k',
+	},
+	{
+		fieldType: FIELD_TYPES.number,
+		fieldName: 'total_estimated_bed_40_mins_100k',
+		fieldDisplayName: 'BEDS IN 40 MINS PER 100K',
+	},
+	{
+		fieldType: FIELD_TYPES.number,
+		fieldName: 'pct_65_over_2018',
+		fieldDisplayName: 'PERCENT POP AGE > 65',
+	},
+	{
+		fieldType: FIELD_TYPES.number,
+		fieldName: 'svi_socioeconomic',
+		fieldDisplayName: 'CDC SVI',
+	},
+	{
+		fieldType: FIELD_TYPES.number,
+		fieldName: 'icuover_max_needed_100k',
+		fieldDisplayName: 'ICU SHORTAGE AT PEAK PER 100K',
 	},
 ]
 
