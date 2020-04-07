4af6300d070bdd67079612d278f31c809687aa0c
davidnmora <davidnmora@gmail.com>
2020-04-07 12:58:34 -0700

    Add hippo animation when each new section is scrolled to :scroll: :runner:

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import 'antd/dist/antd.css';
 import {copyToClipboard, generatePopupHTML} from "./htmlGenerators";
 import {Button} from "antd";
 import {HeartTwoTone, PlusCircleOutlined, SmileOutlined} from '@ant-design/icons';
+import { Scrollama, Step } from 'react-scrollama';
+
 import {GlowButton} from "./GlowButton";
 import {DEFAULT_INPUT_VALUES} from "./constants";
 import {FieldInputRow} from "./FieldInputRow";
@@ -11,7 +13,40 @@ import {DarkStyleSwitch} from "./DarkStyleSwitch";
 import {HippoBar} from "./HippoBar";
 
 
+
+class Graphic extends React.Component {
+  state = {
+    data: 0,
+  };
+  
+  onStepEnter = ({ element, data, direction }) => this.setState({ data });
+  
+  render() {
+    const { data } = this.state;
+    
+    return (
+      <div>
+        <p>data: {data}</p>
+        <Scrollama onStepEnter={this.onStepEnter}>
+          <Step data={1}>
+            <p>step 1</p>
+          </Step>
+          <Step data={2}>
+            <p>step 2</p>
+          </Step>
+        </Scrollama>
+      </div>
+    );
+  }
+}
+
 function App() {
+  const [pageSectionIndex, setPageSectionIndex] = useState(0)
+  const onStepEnter = useCallback(
+    ({ element, data, direction }) => setPageSectionIndex(data),
+    [setPageSectionIndex]
+  )
+  
   const [isDarkStyle, setIsDarkStyle] = useState(true)
   const onToggle = useCallback(
     () => setIsDarkStyle(!isDarkStyle),
@@ -32,34 +67,53 @@ function App() {
   return (
     <div style={{display: 'flex', justifyContent: 'center'}}>
       <div className="App">
-        <HippoBar/>
-        <section className="field-inputs-section">
-          <h2>1. Specify the fields you want in your popup:</h2>
-          <DarkStyleSwitch isDarkStyle={isDarkStyle} onToggle={onToggle}/>
-          {fieldInputValues.map((fieldInputValue, i) => (
-            <FieldInputRow
-              key={i}
-              onChange={newValue => onChangeInput(newValue, i)}
-              fieldInputValue={fieldInputValue}/>)
-          )}
-          <Button size="large" onClick={addNewInputField}>Add another field <PlusCircleOutlined style={{color: 'skyblue'}}/></Button>
-        </section>
+        <HippoBar pageSectionIndex={pageSectionIndex} pageSectionCount={4}/>
         
-        <section>
-          <h2>2.</h2>
-          <GlowButton onClick={() => copyToClipboard(generatePopupHTML(fieldInputValues, isDarkStyle))}>
-            Copy popup HTML to clipboard!
-          </GlowButton>
-        </section>
+        <Scrollama onStepEnter={onStepEnter} offset={0.2}>
+          
+          <Step data={0}>
+            <section className="splash-image">
+              <img src="https://user-images.githubusercontent.com/6570507/78705140-e1c49700-78c1-11ea-827e-74b13bd2e439.gif" alt="popup demo" style={{maxWidth: '100%'}}/>
+            </section>
+          </Step>
+          
+          <Step data={1}>
+            <section className="field-inputs-section">
+              <h2>1. Specify the fields you want in your popup:</h2>
+              <DarkStyleSwitch isDarkStyle={isDarkStyle} onToggle={onToggle}/>
+              {fieldInputValues.map((fieldInputValue, i) => (
+                <FieldInputRow
+                  key={i}
+                  onChange={newValue => onChangeInput(newValue, i)}
+                  fieldInputValue={fieldInputValue}/>)
+              )}
+              <Button size="large" onClick={addNewInputField}>Add another field <PlusCircleOutlined style={{color: 'skyblue'}}/></Button>
+            </section>
+          </Step>
+          
+          <Step data={2}>
+            <section>
+              <h2>2.</h2>
+              <GlowButton onClick={() => copyToClipboard(generatePopupHTML(fieldInputValues, isDarkStyle))}>
+                Copy popup HTML to clipboard!
+              </GlowButton>
+            </section>
+          </Step>
+          
+          <Step data={3}>
+            <section>
+              <h2>3. Paste into the CARTO custom HTML editor!</h2>
+              <p>See photo instructions below.
+                Most importantly, you need to toggle on all the fields you want displayed in the popup before going to the custom editor (toggling on all possible fields is the fool-proof way) <SmileOutlined rotate={180}/>
+              </p>
+              <img src="https://user-images.githubusercontent.com/6570507/78468037-3c929e80-76c8-11ea-8521-266ea1cbf47a.png" alt="custom popup instructions" style={{maxWidth: '100%'}}/>
+              <img src="https://user-images.githubusercontent.com/6570507/78468098-f853ce00-76c8-11ea-847e-77f23fc55cab.png" alt="paste into editor" style={{maxWidth: '100%'}}/>
+            </section>
+          </Step>
         
-        <section>
-          <h2>3. Paste into the CARTO custom HTML editor!</h2>
-          <p>See photo instructions below.
-            Most importantly, you need to toggle on all the fields you want displayed in the popup before going to the custom editor (toggling on all possible fields is the fool-proof way) <SmileOutlined rotate={180}/>
-          </p>
-          <img src="https://user-images.githubusercontent.com/6570507/78468037-3c929e80-76c8-11ea-8521-266ea1cbf47a.png" alt="custom popup instructions" style={{maxWidth: '100%'}}/>
-          <img src="https://user-images.githubusercontent.com/6570507/78468098-f853ce00-76c8-11ea-847e-77f23fc55cab.png" alt="paste into editor" style={{maxWidth: '100%'}}/>
-        </section>
+        
+        
+        </Scrollama>
         
         <p>Made with <HeartTwoTone style={{color: 'red'}} /> by David</p>
       </div>
--- src/HippoBar.js
+++ src/HippoBar.js
@@ -1,11 +1,34 @@
-import React, {useCallback, useState} from 'react';
+import React from 'react';
 
-export const HippoBar = ({pageSectionIndex = 0}) => {
+const BAR_SIDE_MARGIN = 36
+
+export const HippoBar = ({pageSectionIndex, pageSectionCount}) => {
+	const isFirstSection = pageSectionIndex === 0
+	const isLastSection = pageSectionIndex === pageSectionCount - 1
+	
+	const pageProgressPercentage = pageSectionIndex / (pageSectionCount - 1)
+	const hippoLeftPosition = BAR_SIDE_MARGIN + pageProgressPercentage * (window.innerWidth - 2 * BAR_SIDE_MARGIN)
+	const hippoTopPosition = isFirstSection ? '2em' : 8
+	
 	return (
 		<div className="hippo-bar">
-			<div><span className="name">Hippo-popup-mus</span> | CARTO Popup Generator</div>
-			<span className="hippo-emoji" role="img" aria-label="sheep">🦛</span>
-		
+			<div>
+				<span className="hippo-popup-mus" style={{fontSize: isFirstSection ? '3em' : '12px'}}>Hippo-popup-mus</span> | CARTO Popup Generator
+			</div>
+			<span
+				className="hippo-emoji"
+				role="img"
+				aria-label="sheep"
+				style={{
+					left: hippoLeftPosition,
+					top: hippoTopPosition,
+				}}
+			>
+				🦛
+			</span>
+			{isLastSection && <div>
+				🎉
+			</div>}
 		</div>
 	)
 }
