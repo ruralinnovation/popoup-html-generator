3402291cf61ade0157ced06bb922cea1ace106b0
davidnmora <davidnmora@gmail.com>
2020-04-24 16:40:24 -0700

    Updates

--- precache-manifest.87c03599c522de73a8a1303e7ffdf92d.js
+++ precache-manifest.87c03599c522de73a8a1303e7ffdf92d.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "347629f6a5b4e86ba0faefe03e151d68",
+    "revision": "771a3bb7ddd63d6da6c323fd254f5210",
     "url": "/popoup-html-generator/index.html"
   },
   {
-    "revision": "41d710235cebc548d22f",
+    "revision": "a1cf3bb4378e44a190b6",
     "url": "/popoup-html-generator/static/css/2.ad8fe80d.chunk.css"
   },
   {
-    "revision": "7414a5c33055f5a78f4c",
+    "revision": "3e1e315bee9b160c54a9",
     "url": "/popoup-html-generator/static/css/main.a9063966.chunk.css"
   },
   {
-    "revision": "41d710235cebc548d22f",
-    "url": "/popoup-html-generator/static/js/2.8ed09051.chunk.js"
+    "revision": "a1cf3bb4378e44a190b6",
+    "url": "/popoup-html-generator/static/js/2.f411db22.chunk.js"
   },
   {
     "revision": "872d4ad223c39fec1068d862c0b329ff",
-    "url": "/popoup-html-generator/static/js/2.8ed09051.chunk.js.LICENSE.txt"
+    "url": "/popoup-html-generator/static/js/2.f411db22.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7414a5c33055f5a78f4c",
-    "url": "/popoup-html-generator/static/js/main.c4c39c22.chunk.js"
+    "revision": "3e1e315bee9b160c54a9",
+    "url": "/popoup-html-generator/static/js/main.4652c3e2.chunk.js"
   },
   {
     "revision": "7949e2b4f0c937acf00f",
